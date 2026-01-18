fa2aa80eeb3ffc76ebdfa9cc75506acb360e33e5
Kevin Kraemer <kraemer.kevin747@gmail.com>
2026-01-18 15:20:31 +0100

    chore: derive types from convex

--- src/core/hooks.ts
+++ src/core/hooks.ts
@@ -1,21 +1,11 @@
 import { createQuery, createMutation } from "./convex-solid"
 import { api } from "../../convex/_generated/api"
+import type { Doc } from "../../convex/_generated/dataModel"
 
 export const useRestaurants = () => createQuery<Restaurant[]>(api.restaurants.get)
 export const useFriends = () => createQuery<Friend[]>(api.friends.get)
 export const useAddRestaurant = () => createMutation(api.restaurants.add)
 export const useAddFriend = () => createMutation(api.friends.add)
 
-export type Restaurant = {
-    _id: string
-    name: string
-    cuisine: string
-    location: string
-    notes?: string
-    addedBy: string
-}
-
-export type Friend = {
-    _id: string
-    name: string
-}
+export type Restaurant = Doc<"restaurants">
+export type Friend = Doc<"friends">
