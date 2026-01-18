ddafeec1870b703f95677c3ce4b8e6050a383843
Kevin Kraemer <kraemer.kevin747@gmail.com>
2026-01-18 14:30:37 +0100

    chore: add fallbacks

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
-import { For, createSignal, createMemo, Suspense, Show } from "solid-js"
+import { For, createSignal, createMemo, Suspense } from "solid-js"
 import { Header } from "./features/header"
-import { useFriends, useRestaurants, type Restaurant } from "./core/hooks"
+import { useRestaurants, type Restaurant } from "./core/hooks"
 import { Footer } from "./features/footer"
 import { RestaurantCard } from "./features/restaurant-card"
 import { Filters } from "./features/filters"
@@ -14,7 +14,6 @@ export function App() {
     const [show, setShow] = createSignal(false)
 
     const restaurants = useRestaurants()
-    const friends = useFriends()
 
     const filteredRestaurants = createMemo(() =>
         friendFilter() ? restaurants()?.filter((r: Restaurant) => r.addedBy === friendFilter()) : restaurants(),
@@ -27,20 +26,15 @@ export function App() {
             <main class="container mx-auto max-w-7xl flex-1 px-4 pb-8">
                 <Suspense fallback={<Loading />}>
                     <Header count={count} />
-                    <Show when={friends()}>
-                        {(friends) => (
-                            <Filters
-                                friends={friends()}
-                                friendFilter={friendFilter()}
-                                handleFilterChange={setFriendFilter}
-                            >
-                                <Button onClick={() => setShow(true)} class="sm:ml-auto">
-                                    <Plus class="size-4" />
-                                    Add Restaurant
-                                </Button>
-                            </Filters>
-                        )}
-                    </Show>
+                    <Filters
+                        friendFilter={friendFilter()}
+                        handleFilterChange={setFriendFilter}
+                    >
+                        <Button onClick={() => setShow(true)} class="sm:ml-auto">
+                            <Plus class="size-4" />
+                            Add Restaurant
+                        </Button>
+                    </Filters>
                     <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                         <For each={filteredRestaurants()}>
                             {(restaurant) => <RestaurantCard restaurant={restaurant} />}
--- src/features/filters.tsx
+++ src/features/filters.tsx
@@ -2,14 +2,15 @@ import { For, type JSX } from "solid-js"
 import { Badge } from "../ui/badge"
 import { Funnel } from "lucide-solid"
 import { cx } from "../ui/variants"
-import type { Friend } from "../core/hooks"
+import { useFriends, type Friend } from "../core/hooks"
 
 export function Filters(props: {
-    friends: Friend[]
     friendFilter: string | null
     handleFilterChange: (name: string | null) => void
     children: JSX.Element
 }) {
+    const friends = useFriends()
+
     const handleFriendFilter = (f: Friend) => {
         return () => props.handleFilterChange(f.name === props.friendFilter ? null : f.name)
     }
@@ -18,7 +19,7 @@ export function Filters(props: {
         <div class="mb-4 flex flex-col-reverse gap-4 sm:flex-row">
             <div class="flex items-center gap-2">
                 <Funnel class={cx("mr-1 size-4 text-neutral-500", props.friendFilter && "fill-neutral-500")} />
-                <For each={props.friends}>
+                <For each={friends()} fallback={<span>No friends</span>}>
                     {(friend) => (
                         <Badge
                             as="button"
