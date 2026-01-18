47aaa74e27c49709b82f7600b41b3139ab643afe
Kevin Kraemer <kraemer.kevin747@gmail.com>
2026-01-18 15:32:14 +0100

    feat: make header more mobile friendly

--- src/features/header.tsx
+++ src/features/header.tsx
@@ -3,12 +3,12 @@ import { DarkModeToggle } from "./dark-mode-toggle"
 
 export function Header({ count }: { count: () => number }) {
     return (
-        <header class="px-6 py-24 sm:py-32 lg:px-8 dark:bg-neutral-900">
+        <header class="px-6 py-16 sm:py-24 lg:py-32 lg:px-8 dark:bg-neutral-900">
             <div class="mx-auto max-w-2xl text-center">
-                <div class="inline-flex size-16 items-center justify-center rounded-2xl bg-flame-pea-100 text-flame-pea-600">
-                    <UtensilsCrossed class="text-primary size-8" />
+                <div class="inline-flex size-12 sm:size-16 items-center justify-center rounded-2xl bg-flame-pea-100 text-flame-pea-600">
+                    <UtensilsCrossed class="text-primary size-6 sm:size-8" />
                 </div>
-                <h2 class="mt-2 text-5xl font-semibold tracking-tight text-neutral-900 sm:text-7xl dark:text-white">
+                <h2 class="mt-2 text-4xl font-semibold tracking-tight text-neutral-900 sm:text-6xl lg:text-7xl dark:text-white">
                     My next bite
                 </h2>
                 <p class="mt-8 text-lg font-medium text-pretty text-neutral-500 sm:text-xl/8 dark:text-neutral-400">
