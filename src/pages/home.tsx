3f07e0b8e1aa91ca80ba8d5f914cdae1ab8cef81
Kevin Kraemer <41166160+krake747@users.noreply.github.com>
2026-01-19 20:17:14 +0100

    feat: add solid router and not found page (#3)
    * feat: add solid router and not found page
    
    * chore: fmt and lint
    
    * feat: update text on not found

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,14 @@
 import { For, createSignal, createMemo } from "solid-js"
-import { Header } from "./features/header"
-import { useRestaurants, type Restaurant } from "./core/hooks"
-import { Footer } from "./features/footer"
-import { RestaurantCard } from "./features/restaurant-card"
-import { FriendsFilter } from "./features/friends-filter"
-import { AddRestaurantDialog } from "./features/add-restaurant-dialog"
-import { Button } from "./ui/button"
+import { Header } from "../features/header"
+import { useRestaurants, type Restaurant } from "../core/hooks"
+import { Footer } from "../features/footer"
+import { RestaurantCard } from "../features/restaurant-card"
+import { FriendsFilter } from "../features/friends-filter"
+import { AddRestaurantDialog } from "../features/add-restaurant-dialog"
+import { Button } from "../ui/button"
 import { Plus } from "lucide-solid"
 
-export function App() {
+export function Home() {
     const [filter, setFilter] = createSignal<string | null>(null)
     const [show, setShow] = createSignal(false)
 
@@ -21,7 +21,7 @@ export function App() {
     const count = createMemo(() => filteredRestaurants()?.length ?? 0)
 
     return (
-        <div class="flex min-h-screen flex-col text-neutral-900 dark:text-white">
+        <div class="flex min-h-screen flex-col">
             <main class="container mx-auto max-w-7xl flex-1 px-4 pb-8">
                 <Header count={count} />
                 <FriendsFilter filter={filter()} handleFilter={setFilter}>
